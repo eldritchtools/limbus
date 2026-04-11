0cd9c466675feb99766a2f9c130a1345cca461ef
eldritchtools <eldritch.tools@gmail.com>
2026-04-12 01:43:28 +0800

    Remove links to upcoming ids/egos on homepage

--- src/app/page.js
+++ src/app/page.js
@@ -50,10 +50,10 @@ export default function Home() {
         });
 
         return Object.keys(dates).sort((a, b) => {
-            if(a.includes("?")) return -1;
-            if(b.includes("?")) return 1;
+            if (a.includes("?")) return -1;
+            if (b.includes("?")) return 1;
             return b.localeCompare(a);
-        }).slice(0, 10).map(x => [x, dates[x], false]);
+        }).slice(0, 10).map(x => [x, dates[x]]);
     }, [identities, identitiesLoading, egos, egosLoading]);
 
     return <div style={{ display: "flex", justifyContent: "center", marginTop: "2rem", width: "100%", height: "100%" }}>
@@ -117,38 +117,34 @@ export default function Home() {
                     {identitiesLoading || egosLoading ? "Loading..." :
                         <div style={{ maxWidth: "100%", overflowX: "auto", overflowY: "hidden", scrollbarWidth: "thin" }}>
                             <div style={{ display: "flex", gap: "0.5rem" }}>
-                                {latest.map(([date, list, upcoming]) =>
-                                    upcoming ?
-                                        <div key={date} style={{ display: "flex", flexDirection: "column", gap: "0.2rem", alignItems: "start" }}>
-                                            <div>{date}</div>
-                                            <div style={{ display: "flex" }}>
-                                                {list.map(obj => obj.id[0] === "1" ?
+                                {latest.map(([date, list]) =>
+                                    <div key={date} style={{ display: "flex", flexDirection: "column", gap: "0.2rem", alignItems: "start" }}>
+                                        <div>{date}</div>
+                                        <div style={{ display: "flex" }}>
+                                            {list.map(obj => obj.id[0] === "1" ?
+                                                (obj.upcoming ?
                                                     <div key={obj.id} style={{ width: "128px", height: "128px" }}>
-                                                        <IdentityIcon identity={obj} displayName={true} />
+                                                        <IdentityIcon identity={obj} uptie={4} displayName={true} displayRarity={true} includeTooltip={true} />
                                                     </div> :
-                                                    <div key={obj.id} style={{ width: "128px", height: "128px" }}>
-                                                        <EgoIcon ego={obj} displayName={true} />
-                                                    </div>
-                                                )}
-                                            </div>
-                                        </div> :
-                                        <div key={date} style={{ display: "flex", flexDirection: "column", gap: "0.2rem", alignItems: "start" }}>
-                                            <div>{date}</div>
-                                            <div style={{ display: "flex" }}>
-                                                {list.map(obj => obj.id[0] === "1" ?
                                                     <NoPrefetchLink key={obj.id} href={`/identities/${obj.id}`}>
                                                         <div style={{ width: "128px", height: "128px" }}>
                                                             <IdentityIcon identity={obj} uptie={4} displayName={true} displayRarity={true} includeTooltip={true} />
                                                         </div>
-                                                    </NoPrefetchLink> :
+                                                    </NoPrefetchLink>
+                                                ) :
+                                                (obj.upcoming ?
+                                                    <div key={obj.id} style={{ width: "128px", height: "128px" }}>
+                                                        <EgoIcon ego={obj} type={"awaken"} displayName={true} displayRarity={true} includeTooltip={true} />
+                                                    </div> :
                                                     <NoPrefetchLink key={obj.id} href={`/egos/${obj.id}`}>
                                                         <div style={{ width: "128px", height: "128px" }}>
                                                             <EgoIcon ego={obj} type={"awaken"} displayName={true} displayRarity={true} includeTooltip={true} />
                                                         </div>
                                                     </NoPrefetchLink>
-                                                )}
-                                            </div>
+                                                )
+                                            )}
                                         </div>
+                                    </div>
                                 )}
                             </div>
                         </div>
