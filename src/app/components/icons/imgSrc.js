8b3bc23a3792911268f82aa9ecc9bfd7efb70852
eldritchtools <eldritch.tools@gmail.com>
2026-07-19 17:29:00 +0800

    Fixed a crash in rendering some builds, swapped identity and ego images to webp

--- src/app/components/icons/imgSrc.js
+++ src/app/components/icons/imgSrc.js
@@ -2,5 +2,5 @@ import { ASSETS_ROOT } from "@/app/paths";
 
 export function getEgoImgSrc(ego, type) {
     if (ego.upcoming) return `${ASSETS_ROOT}/${ego.src}.png`;
-    return `${ASSETS_ROOT}/egos/${ego.id}_${type}_profile.png`;
+    return `${ASSETS_ROOT}/egos/${ego.id}_${type}_profile.webp`;
 }
