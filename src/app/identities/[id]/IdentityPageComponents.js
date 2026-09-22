f0590bf673f3b5bb330b994de375795c4d9f84a6
eldritchtools <admin@eldritchtools.com>
2026-09-23 02:57:39 +0800

    Added a small guard to prevent crashing when sanity data is missing

--- src/app/identities/[id]/IdentityPageComponents.js
+++ src/app/identities/[id]/IdentityPageComponents.js
@@ -127,34 +127,37 @@ export function SkillsTab({
             null
         }
         <div className="title-text">Sanity</div>
-        <div style={{ display: "flex", flexDirection: "row", flexWrap: "wrap", paddingBottom: "0.25rem", gap: "0.2rem" }}>
-            <div style={{ display: "flex", flexDirection: "column", flex: 1, minWidth: "min(400px, 100%)" }}>
-                <span style={{ fontWeight: "bold" }}>Panic Type</span>
-                <div style={{ display: "flex", gap: "0.5rem" }}>
-                    <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-                        <PanicIcon id={"public_panic"} style={{ width: "96px" }} />
-                        <span style={{ fontWeight: "bold" }}>{sanityData.name}</span>
-                    </div>
-                    <div style={{ display: "flex", flexDirection: "column", alignItems: "start" }}>
-                        {sanityData.lowMoraleDesc.length > 0 && <>
-                            <span>Low Morale</span>
-                            <span>- {sanityData.lowMoraleDesc}</span>
-                        </>}
-                        {sanityData.panicDesc.length > 0 && <>
-                            <span>Panic</span>
-                            <span>- {sanityData.panicDesc}</span>
-                        </>}
+        {sanityData ?
+            <div style={{ display: "flex", flexDirection: "row", flexWrap: "wrap", paddingBottom: "0.25rem", gap: "0.2rem" }}>
+                <div style={{ display: "flex", flexDirection: "column", flex: 1, minWidth: "min(400px, 100%)" }}>
+                    <span style={{ fontWeight: "bold" }}>Panic Type</span>
+                    <div style={{ display: "flex", gap: "0.5rem" }}>
+                        <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+                            <PanicIcon id={"public_panic"} style={{ width: "96px" }} />
+                            <span style={{ fontWeight: "bold" }}>{sanityData.name}</span>
+                        </div>
+                        <div style={{ display: "flex", flexDirection: "column", alignItems: "start" }}>
+                            {sanityData.lowMoraleDesc.length > 0 && <>
+                                <span>Low Morale</span>
+                                <span>- {sanityData.lowMoraleDesc}</span>
+                            </>}
+                            {sanityData.panicDesc.length > 0 && <>
+                                <span>Panic</span>
+                                <span>- {sanityData.panicDesc}</span>
+                            </>}
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div style={{ display: "flex", flexDirection: "column", flex: 1, minWidth: "min(400px, 100%)" }}>
-                <span style={{ fontWeight: "bold", color: "#3B82D0" }}>Base Factors Increasing Sanity</span>
-                {sanityData.add.map((x, i) => <ProcessedText key={i} text={`- ${x}`} />)}
-            </div>
-            <div style={{ display: "flex", flexDirection: "column", flex: 1, minWidth: "min(400px, 100%)" }}>
-                <span style={{ fontWeight: "bold", color: "#D64545" }}>Base Factors Decreasing Sanity</span>
-                {sanityData.sub.map((x, i) => <ProcessedText key={i} text={`- ${x}`} />)}
-            </div>
-        </div>
+                <div style={{ display: "flex", flexDirection: "column", flex: 1, minWidth: "min(400px, 100%)" }}>
+                    <span style={{ fontWeight: "bold", color: "#3B82D0" }}>Base Factors Increasing Sanity</span>
+                    {sanityData.add.map((x, i) => <ProcessedText key={i} text={`- ${x}`} />)}
+                </div>
+                <div style={{ display: "flex", flexDirection: "column", flex: 1, minWidth: "min(400px, 100%)" }}>
+                    <span style={{ fontWeight: "bold", color: "#D64545" }}>Base Factors Decreasing Sanity</span>
+                    {sanityData.sub.map((x, i) => <ProcessedText key={i} text={`- ${x}`} />)}
+                </div>
+            </div> :
+            <span>Sanity Data Unavailable</span>
+        }
     </div>
 }
